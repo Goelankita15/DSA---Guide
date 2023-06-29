d91580f741f8579375486c1e6959b6d486a95768
Goelankita15 <117450446+Goelankita15@users.noreply.github.com>
2023-06-29 10:40:09 +0530

    Create Importanft algo.cpp

---
+++ Importanft algo.cpp
@@ -0,0 +1,9 @@
+//Algorithm x, x-k ;
+u know k then u have to find the x-k if present there or not
+//----------------------------------------------------------
+
+//Two pointer approach
+sort the array
+left  and right == k 
+if greater -> decrese the right as array is sorted
+if smaller -> incresse left as array is sorted
